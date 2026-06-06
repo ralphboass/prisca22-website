e8bcffbf759e1d4f26c07b6e5efb8d0f657affda
ralphboass <rabost15@gmail.com>
2026-06-07 01:25:58 +0200

    Remove basePath - fix white page issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,3 @@
 export function getAssetPath(path: string): string {
-  const basePath = process.env.NODE_ENV === 'production' ? '/prisca22-website' : ''
-  return `${basePath}${path}`
+  return path
 }
--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/prisca22-website',
 }
 
 module.exports = nextConfig
